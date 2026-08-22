72395c0177bc612764797167da9c76b32f963af2
Anjali Kanwar <anjalikanwar006@gmail.com>
2026-08-22 23:04:03 +0530

    initial setup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import dns from "dns";
+import dotenv from "dotenv";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+dotenv.config();
 
 const app=express();
+
+connectDB();
 
 app.listen(4000,()=> {
     console.log("Server started on PORT:4000");
